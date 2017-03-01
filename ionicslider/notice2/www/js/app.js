6ba45fcc528fe7b9ca2cf3e3aea0bcc4a2a41d90
Xudong Hao <xudonghao@Xudongs-iMac.local>
2017-03-01 17:01:51 +0000

    adding local notification on andruid

--- ionicslider/notice2/www/js/app.js
+++ ionicslider/notice2/www/js/app.js
@@ -3,57 +3,106 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic', "ngCordova"])
+var app = angular.module('starter', ['ionic', 'ngCordova']);
 
-  .run(function ($ionicPlatform, $cordovaPush) {
-    $ionicPlatform.ready(function () {
-      var iosConfig = {
-        'badge': true,
-        'sound': true,
-        'alert': true,
-      };
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+app.run(function ($ionicPlatform, $rootScope, $timeout, $window) {
+  $ionicPlatform.ready(function () {
+    var iosConfig = {
+      'badge': true,
+      'sound': true,
+      'alert': true,
+    };
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-        // Don't remove this line unless you know what you are doing. It stops the viewport
-        // from snapping when text inputs are focused. Ionic handles this internally for
-        // a much nicer keyboard experience.
-        cordova.plugins.Keyboard.disableScroll(true);
-
-
-      }
-
-      if (window.cordova) {
-        alert('start');
-        try {
-          $cordovaPush.register(iosConfig).then(function (result) {
-            // Success
-            // 注册成功之后，返回的result是device token，是推送通知时的用户设备标识，
-            // 一般deivce token会与用户信息相关联保存到后台数据库中，
-            var deviceToken = result;
-            alert(11);
-            //$http.post('http://server.com/', {user: 'LiLei', device_token: result})
-          }, function (err) {
-            alert('Registration error: ' + err)
-          });
-        } catch (e) {
-          alert(e);
-        }
-      }
-      if (window.StatusBar) {
-        StatusBar.styleDefault();
-      }
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
 
 
-    });
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+    if (window.cordova) {
+      cordova.plugins.notification.local.on("schedule", function (notification, state) {
+        $rootScope.$broadcast("$cordovaLocalNotification:added", notification);
+      }, $rootScope);
+      cordova.plugins.notification.local.on("trigger", function (notification) {
+        alert(JSON.stringify(notification));
+      }, $rootScope)
+    }
+
   });
+})
 
-app.controller("ExampleController", function ($scope, $cordovaLocalNotification) {
+app.controller('MyCtrl',
+  ['$scope', '$rootScope', '$ionicPlatform', '$cordovaLocalNotification','component',
+    function ($scope, $rootScope, $ionicPlatform, $cordovaLocalNotification,component) {
+      try {
+        $scope.add = function () {
+          try {
+            var alarmTime = new Date();
+
+            alarmTime.setMinutes(alarmTime.getMinutes() + 1);
+            $cordovaLocalNotification.add({
+              id: "1234",
+              date: alarmTime,
+              message: "This is a message",
+              title: "This is a title",
+              autoCancel: true,
+              sound: true
+            }).then(function () {
+
+            });
+          } catch (e) {
+            
+          }
+
+        };
+
+        $scope.isScheduled = function () {
+          try {
+            $cordovaLocalNotification.isScheduled("1234").then(function (isScheduled) {
+              // alert("Notification 1234 Scheduled: " + isScheduled);
+              component.alert("Notification 1234 Scheduled: " + isScheduled,'event status','done');
+            });
+          } catch (e) {
+            
+          }
+
+        }
+      } catch (e) {
+
+      }
 
 
+      $scope.$on("$cordovaLocalNotification:added", function (e, notification) {
+        component.alert('some event has been added','event added','done');
+      });
 
-});
+
+    }]);
 
 
+app.factory('component',[
+  function(){
+    function alert(message,title,button,callback){
+      if(navigator){
+        navigator.notification.alert(
+            message,  // message
+            callback,         // callback
+            title,            // title
+            button                  // buttonName
+        );
+      }else{
+        alert(message)
+      }
+    }
+    return {
+      alert:alert
+    }
+  }]);
